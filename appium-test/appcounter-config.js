3edb69c89489ee526a174c2045289a4a776021d5
thj0309 <thj0309@hotmail.com>
2025-01-27 13:38:22 +0900

    changed

--- appium-test/appcounter-config.js
+++ appium-test/appcounter-config.js
@@ -8,8 +8,8 @@ module.exports = {
   PLATFORM_VERSION: process.env.PLATFORM_VERSION || "18.2",
   DEVICE_NAME: process.env.DEVICE_NAME || "iPhone 16 Pro",
   APP_PATH:
-    process.env.APP_PATH ||
-    "/Users/totohyojuntak/Library/Developer/Xcode/DerivedData/CounterApp-fqmaeslnbhwyflgnlmccxezzwapo/Build/Products/Debug-iphonesimulator/CounterApp.app",
+    process.env.APP_PATH || "/Users/hyojuntak/dev/test/ios-appium/build/CounterApp.xcarchive/Products/Applications/CounterApp.app",
+    //"/Users/totohyojuntak/Library/Developer/Xcode/DerivedData/CounterApp-fqmaeslnbhwyflgnlmccxezzwapo/Build/Products/Debug-iphonesimulator/CounterApp.app",
 
   AUTOMATION_NAME: process.env.AUTOMATION_NAME || "XCUITest",
   MAX_RETRIES: process.env.MAX_RETRIES || 2,
