61098f1aa728445ae14e79b0eceffea40c3a62eb
tworld0309 <thj0309@hotmail.com>
2025-02-04 16:36:21 +0900

    수정

--- appium/5-test-gitlab-appium.sh
+++ appium/5-test-gitlab-appium.sh
@@ -8,14 +8,14 @@ nvm use 18
 echo "🔹 Appium 테스트 실행 중..."
 cd "$(dirname "$0")"  # 현재 스크립트가 위치한 디렉터리로 이동
 
-echo "📌 IP_APP: $IP_APP"
-echo "📌 IP_PLATFORM: $IP_PLATFORM"
-echo "📌 IP_DEVICE_NAME: $IP_DEVICE_NAME"
-echo "📌 IP_PLATFORM_VERSION: $IP_PLATFORM_VERSION"
+# echo "📌 IP_APP: $IP_APP"
+# echo "📌 IP_PLATFORM: $IP_PLATFORM"
+# echo "📌 IP_DEVICE_NAME: $IP_DEVICE_NAME"
+# echo "📌 IP_PLATFORM_VERSION: $IP_PLATFORM_VERSION"
 
-echo "📌 IP_APPIUM_HOST: $IP_APPIUM_HOST"
-echo "📌 IP_APPIUM_PORT: $IP_APPIUM_PORT"
-echo "📌 IP_AUTOMATION_NAME: $IP_AUTOMATION_NAME"
-echo "📌 IP_MAX_RETRIES: $IP_MAX_RETRIES"
+# echo "📌 IP_APPIUM_HOST: $IP_APPIUM_HOST"
+# echo "📌 IP_APPIUM_PORT: $IP_APPIUM_PORT"
+# echo "📌 IP_AUTOMATION_NAME: $IP_AUTOMATION_NAME"
+# echo "📌 IP_MAX_RETRIES: $IP_MAX_RETRIES"
 
 node test-gitlab-runner.js --platform "$IP_PLATFORM" --device "$IP_DEVICE_NAME" --app "$IP_APP" --platformVersion "$IP_PLATFORM_VERSION" --appiumHost "$IP_APPIUM_HOST"  --appiumPort "$IP_APPIUM_PORT"  --automationName "$IP_AUTOMATION_NAME"  --maxTries "$IP_MAX_RETRIES"
